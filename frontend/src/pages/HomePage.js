ba7fa3857bea40679398b9cd36b6ce28825392a1
Azad Wali <waliazad12614@gmail.com>
2026-03-28 15:52:25 +0500

    Create HomePage component for welcome page

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the CCN Crash Course!</h1>
+            <p>Overview of Platform Features:</p>
+            <ul>
+                <li>Interactive Learning Modules</li>
+                <li>Real-time Networking Simulations</li>
+                <li>Expert-led Video Tutorials</li>
+                <li>Hands-on Labs and Projects</li>
+                <li>Comprehensive Assessment Tools</li>
+            </ul>
+        </div>
+    );
+};
+
+export default HomePage;
